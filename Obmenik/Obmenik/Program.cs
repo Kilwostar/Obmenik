8357ddcca7aaa2eb500786e42b3c2dd3b5473225
Kilwostar <35807163+Kilwostar@users.noreply.github.com>
2022-10-13 21:24:16 +0300

    MetersToCentimeters, MetersToKilometers

---
+++ Obmenik/Obmenik/Program.cs
@@ -0,0 +1,14 @@
+﻿double MetersToCentimeters(double num)
+{
+    num = num * 100;
+    return num;
+}
+Console.WriteLine(MetersToCentimeters(1));
+
+
+double MetersToKilometers(double num)
+{
+    num = num / 1000;
+    return num;
+}
+Console.WriteLine(MetersToKilometers(1000));
